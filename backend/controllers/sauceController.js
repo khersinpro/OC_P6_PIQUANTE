fb66e3248bc496319c167b15e717eb99c518339b
khersinpro <khersinpro@gmail.com>
2022-07-10 14:15:02 +0200

    Add like controller

--- backend/controllers/sauceController.js
+++ backend/controllers/sauceController.js
@@ -87,7 +87,7 @@ exports.deleteSauce = (req, res, next) => {
     .then(sauce => {
         // S'il n'y a pas de sauce
         if(!sauce){
-            return res.status(404).json();
+            return res.status(404).json({message: "No results found"});
         };
         // Suppression de l'image de la sauce grâce a fs puis supression de la BDD avec deleteOne()
         const image = sauce.imageUrl.split('/images/')[1];
@@ -100,4 +100,49 @@ exports.deleteSauce = (req, res, next) => {
     .catch(error => {
         res.status(500).json({message: 'An error occurred, try later' , error});
     });
+};
+//*** Like/Dislike une sauce  ***//
+exports.like = (req, res, next) => {
+    // Fonction pour modifier les likes d'une sauce 
+    const updateSauce = (params, response) => {
+        Sauce.updateOne({_id: req.params.id}, params)
+        .then(() => res.status(200).json({message: response}))
+        .catch(error => res.status(500).json({message: 'An error occurred, try later' , error}));
+    };
+
+    try{
+        Sauce.findOne({_id: req.params.id})
+        .then(sauce => {
+            // S'il n'y a pas de sauce
+            if(!sauce){
+                return res.status(404).json({message: "No results found"});
+            };
+            // Si l'utilisateur Like
+            if(req.body.like === 1){
+                //L'operateur $inc de mongoDB permet d'incrémenter/décrémenter une valeur, $push ajoute la valeur a un array
+                const param = {$inc: {likes: +1}, $push: {usersLiked: req.body.userId}};
+                updateSauce(param, "Liked");
+            // Si l'utilsateur Dislike
+            }else if(req.body.like === -1){
+                const param = {$inc: {dislikes: +1}, $push: {usersDisliked: req.body.userId}};
+                updateSauce(param, 'Disliked');
+            // Si l'utilisateur ajoute/retire son like    
+            }else if(req.body.like === 0){
+                // Si il retire son Dislike
+                if(sauce.usersDisliked.includes(req.body.userId)){
+                    // L'operateur $pull de mongoDB permet de retirer une valeur d'un tableau
+                    const param = {$inc: {dislikes: -1}, $pull: {usersDisliked: req.body.userId}};
+                    updateSauce(param, 'Removed');
+                // Si il retire son Like
+                }else if(sauce.usersLiked.includes(req.body.userId)){
+                    const param = {$inc: {likes: -1}, $pull: {usersLiked: req.body.userId}};
+                    updateSauce(param, 'Removed');
+                };
+            };       
+        })
+        .catch(error => res.status(500).json({message: 'An error occurred, try later' , error}));
+    }
+    catch(error){
+        res.status(500).json({message: 'An error occurred, try later' , error});
+    };
 };
